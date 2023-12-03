25be90f1c4888836eb0522c3af1adf7d2818355b
WilsonFirmino <supperwillf@gmail.com>
2023-12-03 19:55:55 -0300

    Iniciado os repository, gerando dados na da API

--- src/main/java/med/voll/api/medico/medico.java
+++ src/main/java/med/voll/api/medico/medico.java
@@ -20,7 +20,7 @@ import med.voll.api.endereco.Endereco;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class medico {
+public class Medico {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,5 +33,14 @@ public class medico {
 
     @Embedded // vai tornar os atributos da classe Endereco campos da tabela Medicos 
     private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados){
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+
+    }
     
 }
