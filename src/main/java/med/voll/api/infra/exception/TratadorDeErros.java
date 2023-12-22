aa48f7f4637bd3f0916695897ec6e559207666c3
WilsonFirmino <supperwillf@gmail.com>
2023-12-21 22:18:58 -0300

    Spring Security

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,10 +1,10 @@
-package med.voll.api.infra;
+package med.voll.api.infra.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;  
 
 import jakarta.persistence.EntityNotFoundException;
 
